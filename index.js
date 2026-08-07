2025d223d2c4b36d9bc05892f9e08787b7ff12bc
FAROUK <faroukfrk075@gmail.com>
2026-08-07 15:37:06 +0100

    Folder_inf_cli

---
+++ index.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+
+
+import path from 'node:path' ;
+import fs from 'fs';
+
+const inputpath = process.argv[2];
+const folderpath = inputpath ? path.resolve(inputpath) : process.cwd();
+
+fs.readdir("test" , function (err , files) {
+  if (err){
+    console.error(err);
+  }
+  else {
+    console.log("directory containes :", files );
+  }
+})
+
+fs.readdir(folderpath , {withFileTypes : true }, function(err , entries ) {
+
+  if (err){
+    console.error(err);
+  }
+  else {
+    const filesC = entries.filter((entry)=>entry.isFile()).length;
+    const dirC = entries.filter((entry)=>entry.isDirectory()).length;
+    console.log(`Folder: ${path.basename(folderpath)}` )
+    console.log(`Path: ${path.folderpath}`)
+    console.log("files: ",filesC);
+    console.log("folders: ",dirC);
+  }
+
+})
+
+
+
